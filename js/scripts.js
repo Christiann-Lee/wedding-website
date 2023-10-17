93df866343d9afde6711235e3b3a07e10d957281
Christian Lee <christian.lee240@gmail.com>
2023-10-17 16:22:21 +1300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -210,15 +210,20 @@ $(document).ready(function () {
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
-        var data = $(this).serialize();
-
+    
+        var data = $(this).serializeArray();
+        var formData = {};
+        data.forEach(function (entry) {
+            formData[entry.name] = entry.value;
+        });
+    
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
+    
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxYNA7dq4ao_C5wDA4QYd2dBoZCdXasawDRjOkCGhdvUejL31m1n-1VPKwcr2iZlnI4Lw/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxYNA7dq4ao_C5wDA4QYd2dBoZCdXasawDRjOkCGhdvUejL31m1n-1VPKwcr2iZlnI4Lw/exec', formData)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -234,6 +239,7 @@ $(document).ready(function () {
                 });
         }
     });
+    
 
 });
 
